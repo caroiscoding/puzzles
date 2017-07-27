5784faae95030514641ec797e690ae334662a28c
Caro <carotonii@icloud.com>
2017-07-27 14:41:04 -0700

    puzzles project

---
+++ puzzles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace puzzles
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
